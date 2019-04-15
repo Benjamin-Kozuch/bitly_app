c6b442d848938cbebd1b8d01280b4442d57407cd
Ben Kozuch <benjamin.k@mvisystems.com>
2019-04-15 17:57:13 -0400

    Refactor and cleanup bitly_api.py

--- app/bitly_api.py
+++ app/bitly_api.py
@@ -8,14 +8,14 @@ def headers(access_token):
 
 
 def user_info(access_token):
-    USER_URL = BASE_URL + '/user'
+    USER_URL = f'{BASE_URL}/user'
     response = requests.get(USER_URL, headers=headers(access_token))
     return response.json()
 
 
 def group_info(access_token):
     group_guid = user_info(access_token)['default_group_guid']
-    BITLINKS_BY_GROUP_URL = BASE_URL + f'/groups/{group_guid}/bitlinks'
+    BITLINKS_BY_GROUP_URL = f'{BASE_URL}/groups/{group_guid}/bitlinks'
     response = requests.get(BITLINKS_BY_GROUP_URL, headers=headers(access_token))
     return response.json()
 
@@ -25,17 +25,23 @@ def average_clicks(country_data):
         sum(country_data)/len(country_data))
 
 
-def average_clicks_per_country(access_token):
+def average_clicks_per_country(access_token, links=None):
     countries = defaultdict(list)
     for link in group_info(access_token)['links']:
         bitlink = link['id']
-        DATA_BY_COUNTRY = BASE_URL + f'/bitlinks/{bitlink}/countries?units=30'
+        DATA_BY_COUNTRY = f'{BASE_URL}/bitlinks/{bitlink}/countries?units=30'
         response = requests.get(DATA_BY_COUNTRY, headers=headers(access_token))
         
         for country_data in response.json()['metrics']:
             countries[country_data['value']].append(country_data['clicks'])
     
-    avg_clicks_per_country = [
+    avg_clicks_per_country = create_avg_click_list(countries)
+    
+    return {'facet':'countries', 'metrics':avg_clicks_per_country}
+
+
+def create_avg_click_list(countries):
+    return [
         {
             'value': country,
             'average_clicks': average_clicks(countries[country])
@@ -43,4 +49,3 @@ def average_clicks_per_country(access_token):
         for country in countries
     ]
 
-    return {'facet':'countries', 'metrics':avg_clicks_per_country}
